63b26b84e638b6ae41b2a5b36114341cbd29baeb
Andrew Sylvester <andrewrsylvester@gmail.com>
2019-10-12 20:43:10 -0500

    Finished Insert

--- src/Main.java
+++ src/Main.java
@@ -14,132 +14,23 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        Scanner in;
-        if (args.length!=2) 
-        {
-            System.out.print("Error Incorrect Arguments:" + Arrays.toString(args));
-            System.exit(0);    
-        } 
-        try 
-		{
-			File input_file = new File(args[0]);
-			in = new Scanner(input_file).useDelimiter(":|\r\\n");
-			File output_file = new File(args[1]);
-			PrintWriter out;
-			out = new PrintWriter(output_file);
-
-			//LazyBinarySearchTree lbst = new LazyBinarySearchTree();
-
-			String operation = "";
-
-			while (in.hasNext()) 
-			{
-				
-				operation = in.next();
-				
-				switch (operation) 
-				{	
-				case "Insert":
-					try 
-					{
-						if(in.findInLine(":") == null)
-						{
-							out.println("Error in Line: " + operation);
-							break;
-						}	
-						//out.println(lbst.insert(in.nextInt()) ? "True" :"False");
-					} 
-					catch (Exception e) 
-					{
-						out.println("Error in insert: IllegalArgumentException raised");
-					}
-					break;
-				case "Delete":
-					try 
-					{
-						//out.println(lbst.delete(in.nextInt()) ? "True" :"False");
-					} 
-					catch (Exception e) 
-					{
-						out.println("Error in Delete: IllegalArgumentException raised");
-					}
-					break;
-				case "FindMin":
-					try 
-					{
-						//out.println(lbst.findMin());
-					} 
-					catch (Exception e) 
-					{
-						out.println("ERROR");
-					}
-					break;
-					
-				case "FindMax":
-					try 
-					{
-						//out.println(lbst.findMax());
-					} 
-					catch (Exception e) 
-					{
-						out.println("ERROR");
-					}
-					break;
-				case "Contains":
-					try 
-					{
-						//out.println(lbst.contains(in.nextInt()) ? "True" :"False");
-					} 
-					catch (Exception e) 
-					{
-						out.println("Error in Contains: IllegalArgumentException raised");
-					}
-					break;
-				case "PrintTree":
-					try 
-					{
-						//out.println(lbst.toString());
-					} 
-					catch (Exception e) 
-					{
-						out.println("ERROR");
-					}
-					break;
-				case "Height":
-					try 
-					{
-						//out.println(lbst.height());
-					} 
-					catch (Exception e) 
-					{
-						out.println("ERROR");
-					}
-					break;
-				case "Size":
-					try 
-					{
-						//out.println(lbst.size());
-					} 
-					catch (Exception e) 
-					{
-						out.println("ERROR");
-					}
-					break;
-				default:
-					out.println("Error in Line: " + operation);
-					//in.nextLine();
-				}
-				if(in.hasNext())
-					in.nextLine();
-			}
-			in.close();
-			out.close();
-		} 
-        catch (Exception e) 
-        {
-			System.out.println("Exception: " + e.getMessage());
-		}
-        
+    	try
+    	{
+	        RedBlackTree<Integer> rbTree = new RedBlackTree();
+	        rbTree.insert(1);
+	        rbTree.insert(2);
+	        rbTree.insert(3);
+	        rbTree.insert(4);
+	        rbTree.insert(5);
+	        rbTree.insert(6);
+	        rbTree.insert(7);
+	        rbTree.insert(8);
+	        System.out.println("Finished");
+    	}
+    	catch (Exception e)
+    	{
+    		System.out.println(e.getMessage());
+    	}
     }
     
     
--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,21 +1,26 @@
-public class RedBlackTree 
+
+public class RedBlackTree<E extends Comparable<E>>
 {
-	private TreeNode root;
+	private static Boolean RED = false;
+	private static Boolean BLACK = true;
+	private Node<E> root;
 	private int treeSize;
 	
-	private static class TreeNode
+	private static class Node<E extends Comparable<E>>
 	{
-		public int key;
-		public TreeNode leftChild;
-		public TreeNode rightChild;
-		public boolean deleted;
+		public E element;
+		public Node<E> leftChild;
+		public Node<E> rightChild;
+		public Node<E> parent;
+		public Boolean color;
 		
-		public TreeNode(int key)
+		public Node(E element, Node<E> parent)
 		{
-			this.key = key;
+			this.element = element;
 			leftChild = null;
 			rightChild = null;
-			deleted = false;
+			this.parent = parent;
+			color = RED;
 		}
 	}
 	
@@ -25,214 +30,205 @@ public class RedBlackTree
 		treeSize = 0;
 	}
 	
-	public boolean insert(int key) throws IllegalArgumentException
+	public Boolean insert(E element) throws NullPointerException
 	{
-		if(key < 1 || key > 99)
-		{
-			throw new IllegalArgumentException("Arguments out of bounds!");
-		}
+		if(element == null)
+			throw new NullPointerException("Element is NULL.");
 		
 		if(root == null)
 		{
-			root = new TreeNode(key);
+			root = new Node<E>(element, null);
+			root.color = BLACK;
 			treeSize++;
 			return true;
 		}
 		
-		return insert(key, root, root);
-	}
-	
-	private boolean insert(int key, TreeNode node, TreeNode parent)
-	{
-		if(node == null)
-		{
-			if(key < parent.key)
-				parent.leftChild = new TreeNode(key);
-			else
-				parent.rightChild = new TreeNode(key);
-			treeSize++;
-			return true;
-		}
+		Node<E> z = insertAtBottom(element, root, root);
 		
-		if(key < node.key)
-		{
-			return insert(key, node.leftChild, node);
-		}
-		else if(key > node.key)
-		{
-			return insert(key, node.rightChild, node);
-		}
-		else if(node.deleted)
-		{
-			node.deleted = false;
-			return true;
-		}
-		else
-		{
+		if(z == null)
 			return false;
+		
+		while(z.parent != null && z.parent.color == RED)
+		{
+			Node<E> uncle = getSibling(z.parent);
+			Node<E> parent = z.parent;
+			Node<E> grandparent = parent.parent;
+			
+			if(uncle == null || uncle.color == BLACK) // Uncle is black
+			{
+				if(grandparent.leftChild != null && grandparent.leftChild.element.compareTo(parent.element) == 0)
+				{
+					if(parent.leftChild != null && parent.leftChild.element.compareTo(z.element) == 0) //Zig left
+					{
+						parent.parent = grandparent.parent;
+						if(parent.parent != null)
+						{
+							if(parent.element.compareTo(parent.parent.element) < 0)
+								parent.parent.leftChild = parent;
+							else
+								parent.parent.rightChild = parent;
+						}
+						
+						grandparent.leftChild = parent.rightChild;
+						if(grandparent.leftChild != null)
+							grandparent.leftChild.parent = grandparent;
+						
+						parent.rightChild = grandparent;
+						grandparent.parent = parent;
+						parent.color = BLACK;
+						grandparent.color = RED;
+						if(parent.parent == null)
+							root = parent;
+					}
+					else //Zig-zag triangle left
+					{
+						grandparent.leftChild = z.rightChild;
+						if(grandparent.leftChild != null)
+							grandparent.leftChild.parent = grandparent;
+						
+						parent.rightChild = z.leftChild;
+						if(parent.rightChild != null)
+							parent.rightChild.parent = parent;
+						
+						z.parent = grandparent.parent;
+						if(parent.parent != null)
+						{
+							if(z.element.compareTo(z.parent.element) < 0)
+								z.parent.leftChild = z;
+							else
+								z.parent.rightChild = z;
+						}
+						
+						z.rightChild = grandparent;
+						grandparent.parent = z;
+						z.leftChild = parent;
+						parent.parent = z;
+						z.color = BLACK;
+						grandparent.color = RED;
+						
+						if(z.parent == null)
+							root = z;
+					}
+				}
+				else
+				{
+					if(parent.leftChild != null && parent.leftChild.element.compareTo(z.element) == 0) //Zig-zag triangle right
+					{
+						grandparent.rightChild = z.leftChild;
+						if(grandparent.rightChild != null)
+							grandparent.rightChild.parent = grandparent;
+						
+						parent.leftChild = z.rightChild;
+						if(parent.leftChild != null)
+							parent.leftChild.parent = parent;
+						
+						z.parent = grandparent.parent;
+						if(parent.parent != null)
+						{
+							if(z.element.compareTo(z.parent.element) < 0)
+								z.parent.leftChild = z;
+							else
+								z.parent.rightChild = z;
+						}
+						
+						z.leftChild = grandparent;
+						grandparent.parent = z;
+						z.rightChild = parent;
+						parent.parent = z;
+						z.color = BLACK;
+						grandparent.color = RED;
+						
+						if(z.parent == null)
+							root = z;
+					}
+					else //Zig right
+					{
+						parent.parent = grandparent.parent;
+						if(parent.parent != null)
+						{
+							if(parent.element.compareTo(parent.parent.element) < 0)
+								parent.parent.leftChild = parent;
+							else
+								parent.parent.rightChild = parent;
+						}
+						
+						grandparent.rightChild = parent.leftChild;
+						if(grandparent.rightChild != null)
+							grandparent.rightChild.parent = grandparent;
+						
+						parent.leftChild = grandparent;
+						grandparent.parent = parent;
+						parent.color = BLACK;
+						grandparent.color = RED;
+						
+						if(parent.parent == null)
+							root = parent;
+					}
+				}
+			}
+			else // Uncle is red, recolor
+			{
+				parent.color = BLACK;
+				grandparent.color = RED;
+				uncle.color = BLACK;
+				z = grandparent;
+			}
+			
 		}
-	}
+		
+		root.color = BLACK;
+		treeSize++;
+		return true;
 
-	public boolean delete(int key) throws IllegalArgumentException
-	{
-		if(key < 1 || key > 99)
-		{
-			throw new IllegalArgumentException("Arguments out of bounds!");
-		}
-		
-		return delete(key, root);
+			
 	}
 	
-	private boolean delete(int key, TreeNode node)
+	private Node<E> insertAtBottom(E element, Node<E> node, Node<E> parent)
 	{
 		if(node == null)
 		{
-			return false;
+			if(element.compareTo(parent.element) < 0)
+				return parent.leftChild = new Node<E>(element, parent);
+			else
+				return parent.rightChild = new Node<E>(element, parent);
 		}
 		
-		if(key < node.key)
+		if(element.compareTo(node.element) < 0)
 		{
-			return delete(key, node.leftChild);
+			return insertAtBottom(element, node.leftChild, node);
 		}
-		else if(key > node.key)
+		else if(element.compareTo(node.element) > 0)
 		{
-			return delete(key, node.rightChild);
-		}
-		else if(!node.deleted)
-		{
-			node.deleted = true;
-			return true;
+			return insertAtBottom(element, node.rightChild, node);
 		}
 		else
 		{
+			return null;
+		}
+	}
+	
+	private Node<E> getSibling(Node<E> node)
+	{
+		if(node.parent.leftChild != null && node.parent.leftChild.element.compareTo(node.element) == 0) // is left, get right
+		{
+			return node.parent.rightChild;
+		}
+		else // is left, get right
+		{
+			return node.parent.leftChild;
+		}
+	}
+	
+	public Boolean contains(Comparable<E> object)
+	{
+		if(object == null)
 			return false;
-		}
-	}
-	
-	public int findMin()
-	{
-		return findMin(root);
-	}	
-	
-	//infix
-	private int findMin(TreeNode node)
-	{
-		if(node != null)
-		{
-			int leftMin = findMin(node.leftChild);
-			if(leftMin > -1)
-				return leftMin;
-			
-			if(!node.deleted)
-				return node.key;
-			
-			int rightMin = findMin(node.rightChild);
-			if(rightMin > -1)
-				return rightMin;
-		}
-		return -1;
-	}
-	
-	
-	public int findMax()
-	{
-		return findMax(root);
-	}
-	
-	private int findMax(TreeNode node)
-	{
-		if(node != null)
-		{
-			int rightMin = findMax(node.rightChild);
-			if(rightMin > -1)
-				return rightMin;
-			
-			if(!node.deleted)
-				return node.key;
-			
-			int leftMin = findMax(node.leftChild);
-			if(leftMin > -1)
-				return leftMin;
-		}
-		return -1;
-	}
-	
-	public boolean contains(int key) throws IllegalArgumentException
-	{
-		if(key < 1 || key > 99)
-		{
-			throw new IllegalArgumentException("Arguments out of bounds!");
-		}
-		
-		return contains(key, root);
-	}
-	
-	private boolean contains(int key, TreeNode node)
-	{
-		if(node == null)
-		{
-			return false;
-		}
-		
-		if(key < node.key)
-		{
-			return contains(key, node.leftChild);
-		}
-		else if(key > node.key)
-		{
-			return contains(key, node.rightChild);
-		}
-		else if(!node.deleted)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return false;
 	}
 	
 	public String toString()
 	{
-		return toString(root, "");
+		return "";
 	}
-	
-	private String toString(TreeNode node, String output)
-	{
-		if(node != null)
-		{
-			if(node.deleted)
-			{
-				output = output + "*";
-			}
-			output = output + node.key + " " + toString(node.leftChild, output) + toString(node.rightChild, output);
-			return output;
-		}
-		else
-		{
-			return "";
-		}
-	}
-	
-	public int height()
-	{
-		return height(root);
-	}
-	
-	private int height(TreeNode node)
-	{
-		if(node == null)
-		{
-			return -1;
-		}
-		else
-		{
-			return 1 + Math.max(height(node.leftChild), height(node.rightChild));
-		}
-	}
-	
-	public int size()
-	{
-		return treeSize;
-	}
+
 }
